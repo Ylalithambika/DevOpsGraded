98ef9dd30754b67241a7722e33cbffe96b5ee46b
student <lalithambika.y@hcl.com>
2021-04-20 07:07:51 +0000

    First Commit

---
+++ GradedAssignmentJenkins/src/main/java/com/lalitha/graded/GradedAssignmentJenkins/GradedAssignmentJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.lalitha.graded.GradedAssignmentJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedAssignmentJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedAssignmentJenkinsApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ GradedAssignmentJenkins/src/test/java/com/lalitha/graded/GradedAssignmentJenkins/GradedAssignmentJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalitha.graded.GradedAssignmentJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradedAssignmentJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
